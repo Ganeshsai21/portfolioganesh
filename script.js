4fcaddd3c66b6fc5d23546fb34d461d324e378f8
Ganeshsai21 <147413913+Ganeshsai21@users.noreply.github.com>
2025-06-09 20:17:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+  function toggleMenu() {
+    const menu = document.querySelector('.navbar-right');
+    if (menu.style.display === 'flex') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'flex';
+      menu.style.flexDirection = 'column'; // make it stack on small screens
+    }
+      menu.classList.toggle('active');
+  }
+
 
